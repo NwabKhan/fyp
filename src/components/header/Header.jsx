ff5e4cda90d11da95a41d7ef80e5fa9298cbee4e
NwabKhan <m.junaid.89630@gmail.com>
2023-04-05 07:47:43 +0500

    added header user profile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,81 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
-import {NavLink} from 'react-router-dom'
-import css from '../../css/header.module.css'
-
+import { useNavigate } from "react-router-dom";
+import css from "../../css/header.module.css";
+import { useUserAuthContext } from "../../context/UserAuthContet";
+import {
+  Avatar,
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+  Tooltip,
+  Typography,
+} from "@mui/material";
 const Header = () => {
+  const { userName, userEmail, userDP, logOut } = useUserAuthContext();
+
+  useEffect(() => {
+    console.log("Data receiving in Header:", userName, userEmail, userDP);
+  }, [userName, userEmail, userDP]);
+
+  const navigate = useNavigate()
+  const settings = ["Profile", "Logout"];
+  
+  const [anchorElUser, setAnchorElUser] = useState(null);
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+  const handleSettingsClick = (e)=>{
+    const clickedTab = e.target.innerHTML ;
+    switch(clickedTab){
+      case "Profile" :
+        navigate("/home")
+        break
+      case "Logout" :
+        logOut();
+        break
+      default:
+        console.log("Defalut goes here")
+    }
+    setAnchorElUser(null);
+  }
   return (
-    <div className = {`${css.header}`}>
-      <NavLink className="text-dark" to="/">
-        <h5>LOGO</h5>
-      </NavLink>
+    <div className={`${css.header}`} style={{ position: 'fixed', top: 0, left: 0, right: 0 }}>
+      <Box>
+        <Tooltip title="Open settings">
+          <IconButton onClick={handleOpenUserMenu}>
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+          </IconButton>
+        </Tooltip>
+        <Menu
+          sx={{ mt: "45px" }}
+          id="menu-appbar"
+          anchorEl={anchorElUser}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          open={Boolean(anchorElUser)}
+          onClose={handleCloseUserMenu}
+        >
+          {settings.map((setting) => (
+            <MenuItem key={setting} onClick={(e)=>handleSettingsClick(e)}>
+              <Typography>{setting}</Typography>
+            </MenuItem>
+          ))}
+        </Menu>
+      </Box>
       <Navbar />
     </div>
   );
--- src/context/UserAuthContet.jsx
+++ src/context/UserAuthContet.jsx
@@ -1,63 +1,98 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { auth } from './../firebase';
+import { auth } from "./../firebase";
 import {
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    sendEmailVerification,
-    signOut,
-    onAuthStateChanged,
-    GoogleAuthProvider,
-    signInWithPopup
-} from 'firebase/auth'
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  sendEmailVerification,
+  signOut,
+  onAuthStateChanged,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
 const UserAuthContext = createContext();
 
 export const UserAuthContextProvider = ({ children }) => {
-    const [user, setUser] = useState('') //to check wich user is active
-    const [timeActive, setTimeActive] = useState(false); //time after sending confirmation email
-    //Signup using emailId and password
-    const signUp = (email, password)=>{
-        return createUserWithEmailAndPassword(auth,email, password)
-    }
+  const [user, setUser] = useState(""); //to check wich user is active
+  const [timeActive, setTimeActive] = useState(false); //time after sending confirmation email
 
-    //to send the verification email to user when he enter his signUp
-    const emailVerification = ()=>{
-        return sendEmailVerification(auth.currentUser)
-    }
+  // after login/signup using goole browserPopupRedirectResolver, storing user informatin
+  const [userName, setUserName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [userDP, setUserDP] = useState("");
+  //Signup using emailId and password
+  const signUp = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
-    //LogIn using emailId and password
-    const logIn = (email, password)=>{
-        return signInWithEmailAndPassword(auth, email, password)
-    }
+  //to send the verification email to user when he enter his signUp
+  const emailVerification = () => {
+    return sendEmailVerification(auth.currentUser);
+  };
 
-    //Logout the user
-    const logOut = ()=>{
-        return signOut(auth)
-    }
+  //LogIn using emailId and password
+  const logIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
-    //to signin with google
-    const googleSignin = ()=>{
-        const googleAuthProvider = new GoogleAuthProvider()
-        return signInWithPopup(auth,googleAuthProvider)
-    }
+  //Logout the user
+  const logOut = () => {
+    return signOut(auth);
+  };
 
-    //to notify that a particular user is logged in
-    useEffect(()=>{
-        const unsubscribe = onAuthStateChanged(auth, (currentUser)=>{
-            //when auth change, set user to current user
-            setUser(currentUser)
-        })
-        //we do this becuase when a component is unmounted we don't want this
-        //So to clean up we use this method
-        return ()=>{
-            unsubscribe()
-        }
-    })
+  //to signin with google
+  const googleSignin = async () => {
+    const googleAuthProvider = new GoogleAuthProvider();
+    return signInWithPopup(auth, googleAuthProvider)
+    .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // console.log("Google Autg Provider credential:",credential, "and token is:", token )
+        // after signing u storing user info
+        var user = result.user;
+        setUserName(user.displayName);
+        setUserEmail(user.email);
+        setUserDP(user.photoURL);
+        // console.log("user_provider="+user.displayName+" user_email="+user.email+" user_dp="+user.photoURL+" user_verification="+user.emailVerified+" uid="+user.uid);
+      })
+      .catch((error) => {
+        console.log("error=" + error);
+      });
+  };
+
+  //to notify that a particular user is logged in
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      //when auth change, set user to current user
+      setUser(currentUser);
+    });
+    //we do this becuase when a component is unmounted we don't want this
+    //So to clean up we use this method
+    return () => {
+      unsubscribe();
+    };
+  });
   return (
-    <UserAuthContext.Provider value={{user, signUp, logIn, emailVerification, logOut, timeActive, setTimeActive, googleSignin}}>{children}</UserAuthContext.Provider>
+    <UserAuthContext.Provider
+      value={{
+        user,
+        signUp,
+        logIn,
+        emailVerification,
+        logOut,
+        timeActive,
+        setTimeActive,
+        googleSignin,
+        userName,
+        userEmail,
+        userDP
+      }}
+    >
+      {children}
+    </UserAuthContext.Provider>
   );
 };
 
 //custome useContext hook
-export const useUserAuthContext = ()=>{
-    return useContext(UserAuthContext) 
-}
+export const useUserAuthContext = () => {
+  return useContext(UserAuthContext);
+};
